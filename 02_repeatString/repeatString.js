df95a885fe207df594f2481534c1f4daa426cf46
catherineRotairo <catherine.g.rotairo@gmail.com>
2022-03-13 19:58:21 +1300

    repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(message, repeats) {
+    let returnMessage  = '';
+    if (repeats < 0) {return 'ERROR'};
+    for (let i = 0; i < repeats; i++){
 
+        returnMessage += message;
+    }
+    return returnMessage
 };
 
 // Do not edit below this line
