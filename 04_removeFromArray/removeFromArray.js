472f6fc79b12f55c084074889dc61a2e2849e3e7
catherineRotairo <catherine.g.rotairo@gmail.com>
2022-04-01 14:35:10 +1300

    update answer to exercise04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (anArray, ...args) {
+    for (const arg of args) {
+        const index = anArray.indexOf(arg);
+        if (index === -1 && arg !== -1) {
+            continue;
+        }
+        anArray.splice(index, 1);
+    };
+    return anArray;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
