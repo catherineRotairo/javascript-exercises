5ae3b88541eba05002e5213f95e130adc82bbd81
catherineRotairo <catherine.g.rotairo@gmail.com>
2022-04-09 18:41:39 +1200

    upload answers to exercise05 sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
 const sumAll = function() {
-
+    let args = [...arguments];
+    for (const arg of args){
+        if (typeof(arg) === `number` && arg < 0){
+            return `ERROR`;
+        }
+        else if (typeof(arg) !== `number`){
+            return `ERROR`;
+        }
+        else {
+            continue;
+        }
+    };
+    let total = 0;
+    for (let i = Math.min(...arguments); i <= Math.max(...arguments); i++){
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
